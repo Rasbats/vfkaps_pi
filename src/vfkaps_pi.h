6c7bdaa12fdc45f2786762b2786f8a7435f1cc4a
Rasbats <miker@members.swis.net>
2017-10-24 09:55:58 +0100

    Signed-off-by: Rasbats <miker@members.swis.net>

--- src/vfkaps_pi.h
+++ src/vfkaps_pi.h
@@ -44,8 +44,8 @@
 #define     MY_API_VERSION_MAJOR    1
 #define     MY_API_VERSION_MINOR    13
 
-#define     PLUGIN_VERSION_MAJOR    0
-#define     PLUGIN_VERSION_MINOR    7
+#define     PLUGIN_VERSION_MAJOR    1
+#define     PLUGIN_VERSION_MINOR    0
 
 
 
