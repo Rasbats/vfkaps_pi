fd23b245066b0208ea77dd28e86839364a3ccf42
Rasbats <miker@members.swis.net>
2025-11-15 15:02:29 +0000

    v4.1.2

--- Plugin.cmake
+++ Plugin.cmake
@@ -32,7 +32,7 @@ set(OCPN_RELEASE_REPO
 # -------  Plugin setup --------
 #
 set(PKG_NAME vfkaps_pi)
-set(PKG_VERSION  4.1.1)
+set(PKG_VERSION  4.1.2)
 set(PKG_PRERELEASE "")  # Empty, or a tag like 'beta'
 
 set(DISPLAY_NAME vfkaps)    # Dialogs, installer artifacts, ...
